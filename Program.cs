a643e9c56c9ded4d84bef228a781eec2505a2897
Ksuvka <ksuhareva06@gmail.com>
2026-05-25 20:21:56 +0300

    Добавлена автоматическая миграция базы данных при запуске

--- Program.cs
+++ Program.cs
@@ -31,6 +31,12 @@ builder.Services.AddScoped<AuthService>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
